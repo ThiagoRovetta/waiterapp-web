ee4f6735832458b4848065deec0604bb497a7386
ThiagoRovetta <rovetta.thiago@gmail.com>
2023-02-28 22:15:34 -0300

    feat: create new icon components.

---
+++ src/components/Icons/CheckboxOffIcon.tsx
@@ -0,0 +1,17 @@
+export function CheckboxOffIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="3"
+        y="3"
+        width="18"
+        height="18"
+        rx="5"
+        stroke="#666666"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
